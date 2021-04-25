a6d40cbe62b1f94e7e031e49ad08e4da9728da67
anatolich <anatolich.work.space@gmail.com>
2021-04-25 20:41:35 +0300

    Lesson 3. Conditional statements. Practice

--- index.php
+++ index.php
@@ -4,20 +4,257 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://unpkg.com/mustard-ui@latest/dist/css/mustard-ui.min.css">
     <link rel="stylesheet" href="css/style.css"
     <title>Lesson 3</title>
 </head>
 <body>
-    <h1>Условные операторы</h1>
 
     <div class="container">
-        <label for="age">Enter age: </label>
-        <input type="text" class="age">
-        <button>Save</button>
+        <h1>Условные операторы</h1>
+
+        <section>
+            <h3>Task 1.</h3>
+
+            <div class="form-control">
+                Input i-1 <input type="text" class="i-1">
+            </div>
+
+            <button class ="button-primary b-1">Task-1</button>
+            <div class="out-1"></div>
+        </section>
+
+        <section>
+            <h3>Task 2.</h3>
+
+            <button class ="button-primary b-2">Task-2</button>
+            <div class="out-2"></div>
+        </section>
+
+        <section>
+            <h3>Task 3.</h3>
+
+            <div class="form-control">
+                Input i-31
+                <input type="number" class="i-31">
+            </div>
+
+            <div class="form-control">
+                Input i-32
+                <input type="number" class="i-32">
+            </div>
+
+            <button class ="button-primary b-3">Task-3</button>
+            <div class="out-3"></div>
+        </section>
+
+        <section>
+            <h3>Task 4.</h3>
+
+            <div class="form-control">
+                Input i-4
+                <input type="number" class="i-4">
+            </div>
+
+            <button class ="button-primary b-4">Task-4</button>
+
+            <div class="out-4"></div>
+        </section>
+
+        <section>
+            <h3>Task 5.</h3>
+
+            <div class="form-control">
+                Input i-5
+                <input type="number" class="i-5">
+            </div>
+
+            <button class ="button-primary b-5">Task-5</button>
+
+            <div class="out-5"></div>
+        </section>
+
+        <section>
+            <h3>Task 6.</h3>
+
+            <div class="form-control">
+                Input i-6
+                <input type="number" class="i-6">
+            </div>
+
+            <button class ="button-primary b-6">Task-6</button>
+
+            <div class="out-6"></div>
+        </section>
+
+        <section>
+            <h3>Task 7.</h3>
+
+            <div class="form-control">
+                Input i-71
+                <input type="number" class="i-71">
+            </div>
+
+            <div class="form-control">
+                Input i-72
+                <input type="number" class="i-72">
+            </div>
+
+            <button class ="button-primary b-7">Task-7</button>
+
+            <div class="out-7"></div>
+        </section>
+
+        <section>
+            <h3>Task 8.</h3>
+
+            <div class="form-control">
+                Select s-8
+                <select class="s-8">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select>
+            </div>
+
+            <button class ="button-primary b-8">Task-8</button>
+
+            <div class="out-8"></div>
+        </section>
+
+        <section>
+            <h3>Task 9.</h3>
+
+            <div class="form-control">
+                Input i-9
+                <input type="number" class="i-9">
+            </div>
+
+            <button class ="button-primary b-9">Task-9</button>
+
+            <div class="out-9"></div>
+        </section>
+
+        <section>
+            <h3>Task 10.</h3>
+
+            <div class="form-control">
+                Select s-10
+                <select class="s-10">
+                    <option value="six">six</option>
+                    <option value="seven">seven</option>
+                    <option value="eight">eight</option>
+                    <option value="nine">nine</option>
+                </select>
+            </div>
+
+            <button class ="button-primary b-10">Task-10</button>
+
+            <div class="out-10"></div>
+        </section>
+
+        <section>
+            <h3>Task 11.</h3>
+
+            <div class="form-control">
+                Select s-11
+                <select class="s-11">
+                    <option value="six">six</option>
+                    <option value="seven">seven</option>
+                    <option value="eight">eight</option>
+                    <option value="nine">nine</option>
+                </select>
+            </div>
+
+            <div class="out-11"></div>
+        </section>
+
+        <section>
+            <h3>Task 12.</h3>
+
+            <div class="form-control">
+                Input i-12
+                <input type="text" class="i-12">
+            </div>
+
+            <button class ="button-primary b-12">Task-12</button>
+
+            <div class="out-12"></div>
+        </section>
+
+        <section>
+            <h3>Task 13.</h3>
+
+            <div class="form-control">
+                Input i-13
+                <input type="number" class="i-13">
+            </div>
+
+            <button class ="button-primary b-13">Task-13</button>
+
+            <div class="out-13"></div>
+        </section>
+
+        <section>
+            <h3>Task 14.</h3>
+            <div class="form-control">
+                Input i-141
+                <input type="number" class="i-141">
+            </div>
+
+            <div class="form-control">
+                Input i-142
+                <input type="number" class="i-142">
+            </div>
+
+
+            <div class="form-control">
+                 Select s-143
+                <select class="s-143">
+                    <option value="+">+</option>
+                    <option value="-">-</option>
+                    <option value="*">*</option>
+                    <option value="/">/</option>
+                </select>
+            </div>
+
+            <button class ="button-primary b-14">Task-14</button>
+
+            <div class="out-14"></div>
+        </section>
+
+        <section>
+            <h3>Task 15.</h3>
+
+            <div class="form-control">
+                Select s-151
+                <select class="s-151">
+                    <option value="0">0</option>
+                    <option value="1">1</option>
+                </select>
+            </div>
+
+            <div class="form-control">
+                Select s-152
+                <select class="s-152">
+                    <option value="0">0</option>
+                    <option value="1">1</option>
+                </select>
+            </div>
+
+            <div class="form-control">
+                Select s-153
+                <select class="s-153">
+                    <option value="&&">&&</option>
+                    <option value="||">||</option>
+                </select>
+            </div>
+
+            <button class ="button-primary b-15">Task-15</button>
+
+            <div class="out-15"></div>
+        </section>
     </div>
-
-
-
 
     <script src="js/script.js"></script>
 </body>
