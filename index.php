2ec208879c1b8de0f386921f3f13f4e8fe1a5e05
anatolich <anatolich.work.space@gmail.com>
2021-04-25 19:53:20 +0300

    Start project

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto&display=swap">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h1>стартуем нашу первую программу</h1>
+    </div>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
