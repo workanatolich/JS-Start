6e9653b203359cfeacb64d9b40160581e63e4ffa
anatolich <anatolich.work.space@gmail.com>
2021-05-01 21:29:21 +0300

    Lesson 7. Functions. Theory

--- index.php
+++ index.php
@@ -6,64 +6,14 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <link rel="stylesheet" href="css/mustard-ui.min.css">
     <link rel="stylesheet" href="css/style.css"
-    <title>Lesson 6</title>
+    <title>Lesson 7</title>
 </head>
 <body>
         <div class="container">
-            <h1 class="text-center">Working with nested loops</h1>
+            <h1 class="text-center">Function</h1>
 
-            <section>
-                <button class = "button-primary b-1">Task 1</button>
-                <p class = "out-1"></p>
-            </section>
-
-            <section>
-                <button class="button-primary b-2">Task 2</button>
-                <p class="out-2"></p>
-            </section>
-
-            <section>
-                <button class="button-primary b-3">Task 3</button>
-                <p class="out-3"></p>
-            </section>
-
-            <section>
-                <button class="button-primary b-4">Task 4</button>
-                <p class="out-4"></p>
-            </section>
-
-            <section>
-                <button class="button-primary b-5">Task 5</button>
-                <p class="out-5"></p>
-            </section>
-
-            <section>
-                <button class="button-primary b-6">Task 6</button>
-                <p class="out-6"></p>
-            </section>
-
-            <section>
-                <button class="button-primary b-7">Task 7</button>
-                <p class="out-7"></p>
-            </section>
-
-            <section>
-                <button class="button-primary b-8">Task 8</button>
-                <p class="out-8"></p>
-            </section>
-
-            <section>
-                <button class="button-primary b-9">Task 9</button>
-                <p class="out-9"></p>
-            </section>
-
-            <section>
-                <button class="button-primary b-10">Task 10</button>
-                <p class="out-10"></p>
-            </section>
-
-
-
+            <button class="f-1">Func 1</button>
+            <button class="f-2">Func 2</button>
 
         </div>
         
--- js/script.js
+++ js/script.js
@@ -1,137 +1,45 @@
-function f1() {
-    let out = document.querySelector('.out-1');
+let f1 = document.querySelector('.f-1');
 
-    for(let i = 0; i < 3; i++) {
-        for(let k = 0; k < 3; k++) {
-            out.innerHTML += '*';
-        }
-        out.innerHTML += '_';
-    }
+function one() {
+    console.log("It's work");
 }
-document.querySelector('.b-1').onclick = f1;
 
-function f2() {
-    let out = document.querySelector('.out-2');
+one();
 
-    for(let i = 1; i < 4; i++) {
-        out.innerHTML += `<br> ${i} <br>`;
-        for(let k = 0; k < 3; k++) {
-            out.innerHTML += `*_`;
-        }
-    }
+f1.onclick = one;
+
+console.log(1+one()); // NaN
+console.log(one()); // underfined
+
+function two() {
+    console.log('work 1111');
+    return 54;
 }
-document.querySelector('.b-2').onclick = f2;
 
-function f3() {
-    let out = document.querySelector('.out-3');
+console.log(1+two());
 
-    for(let i = 1; i <= 4; i++) {
-        for(let k = 0; k < 3; k++) {
-            out.innerHTML += '*_';
-        }
-        out.innerHTML += '<br>';
-    }
+let a = 8;
+let b = 9;
+
+function multi() {
+    console.log(a*b);
+    return a * b;
 }
-document.querySelector('.b-3').onclick = f3;
 
-function f4() {
-    let out = document.querySelector('.out-4');
+let c1 = multi();
+let c2 = 10 * multi();
+console.log(c1, c2);
 
-    for(let i = 1; i <= 3; i++) {
-        out.innerHTML += i + '_';
-        for(let k = 1; k < 6; k++) {
-            out.innerHTML += k + ' ';
-        }
-    }
+function multi2(x,y) {
+    return x * y;
 }
-document.querySelector('.b-4').onclick = f4;
 
-function f5() {
-    let out = document.querySelector('.out-5');
+console.log(multi2(4,5));
 
-    for(let i = 1; i <= 3; i++) {
-        for(let k = 0; k < 6; k++) {
-            if(k % 2 !== 0 && k!== 0){
-                out.innerHTML += '0';
-            }
-            else {
-                out.innerHTML += '1';
-            }
-        }
-        out.innerHTML += '<br>';
-    }
+document.querySelector('.f-2').onclick = () => {
+    console.log ('*****');
 }
-document.querySelector('.b-5').onclick = f5;
 
-function f6() {
-    let out = document.querySelector('.out-6');
-
-    for(let i = 1; i <= 3; i++) {
-        for(let k = 0; k < 6; k++) {
-            if(k % 3 === 2) {
-                out.innerHTML += 'x';
-            }
-            else if(k % 2 !== 0) {
-                out.innerHTML += '0';
-            }
-            else {
-                out.innerHTML += '1';
-            }
-        }
-        out.innerHTML += '<br>';
-    }
+document.querySelector('.f-2').onclick = function() {
+    console.log ('123*****');
 }
-document.querySelector('.b-6').onclick = f6;
-
-function f7() {
-    let out = document.querySelector('.out-7');
-
-    for(let i = 0; i < 5; i++) {
-        for(k = 0; k < i; k++) {
-            out.innerHTML += '*';
-        }
-        out.innerHTML += '<br>';
-    }
-}
-document.querySelector('.b-7').onclick = f7;
-
-function f8() {
-    let out = document.querySelector('.out-8');
-
-    for(let i = 5; i > 0; i--) {
-        for(k = 0; k < i; k++) {
-            out.innerHTML +='*';
-        }
-        out.innerHTML += '<br>';
-    }
-}
-document.querySelector('.b-8').onclick = f8;
-
-function f9() {
-    let out = document.querySelector('.out-9');
-
-    for(let i = 1; i < 7; i++) {
-        for(k = 1; k < i; k++) {
-            out.innerHTML += `${k} `;
-        }
-        out.innerHTML += '<br>';
-    }
-}
-document.querySelector('.b-9').onclick = f9;
-
-function f10() {
-    let out = document.querySelector('.out-10');
-
-    for(let i = 0; i < 6; i++) {
-        for(let k = 0; k <= 9; k++) {
-            if(k === 9) {
-                out.innerHTML += `${i+1}0`;
-                break;
-            }
-            out.innerHTML += `${i}${k+1} `;
-        }
-        out.innerHTML += '<br>'
-
-    }
-}
-document.querySelector('.b-10').onclick = f10;
