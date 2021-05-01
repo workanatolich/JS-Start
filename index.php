2e64fa21be9d86486b1c82050e9e2ed265ef780a
anatolich <anatolich.work.space@gmail.com>
2021-05-01 18:28:22 +0300

    Lesson 6. Nested loops. Theory

--- index.php
+++ index.php
@@ -6,138 +6,15 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <link rel="stylesheet" href="css/mustard-ui.min.css">
     <link rel="stylesheet" href="css/style.css"
-    <title>Lesson 5</title>
+    <title>Lesson 6</title>
 </head>
 <body>
         <div class="container">
-            <h1 class="text-center">Working with cycles</h1>
+            <h1 class="text-center">Working with nested loops</h1>
 
-            <section>
-                <button class="button-primary b-1">Task 1</button>
-                <div class="out-1"></div>
-            </section>
+            <div class="out"></div>
 
-            <section>
-                <button class="button-primary b-2">Task 2</button>
-                <div class="out-2"></div>
-            </section>
-
-            <section>
-                <button class="button-primary b-3">Task 3</button>
-                <div class="out-3"></div>
-            </section>
-
-            <section>
-                <button class="button-primary b-4">Task 4</button>
-                <div class="out-4"></div>
-            </section>
-
-            <section>
-                <button class="button-primary b-5">Task 5</button>
-                <div class="out-5"></div>
-            </section>
-
-            <section>
-                <div class="form-control">
-                    Input i-6
-                    <input type="text" class="i-6">
-                </div>
-                <button class="button-primary b-6">Task 6</button>
-                <div class="out-6"></div>
-            </section>
-
-            <section>
-                <div class="form-control">
-                    Input i-7
-                    <input type="text" class="i-7">
-                </div>
-                <button class="button-primary b-7">Task 7</button>
-                <div class="out-7"></div>
-            </section>
-
-            <section>
-                <div class="form-control">
-                    Input i-81
-                    <input type="text" class="i-81">
-                </div>
-
-                <div class="form-control">
-                    Input i-82
-                    <input type="text" class="i-82">
-                </div>
-
-                <button class="button-primary b-8">Task 8</button>
-                <div class="out-8"></div>
-            </section>
-
-            <section>
-                <div class="form-control">
-                    Input i-91
-                    <input type="text" class="i-91">
-                </div>
-
-                <div class="form-control">
-                    Input i-92
-                    <input type="text" class="i-92">
-                </div>
-
-                <button class="button-primary b-9">Task 9</button>
-                <div class="out-9"></div>
-            </section>
-
-            <section>
-                <button class="button-primary b-10">Task 10</button>
-                <div class="out-10"></div>
-            </section>
-
-            <section>
-                <div class="div-11">one</div>
-                <div class="div-11">3</div>
-                <div class="div-11">4</div>
-                <div class="div-11">two</div>
-
-                <button class="button-primary b-11">Task 11</button>
-                <div class="out-11"></div>
-            </section>
-
-            <section>
-                <div class="div-12">1</div>
-                <div class="div-12">2</div>
-
-                <button class="button-primary b-12">Task 12</button>
-            </section>
-
-            <section>
-
-                <input type="text" class="i-13" value="1">
-                <input type="text" class="i-13" value="2">
-                <input type="text" class="i-13" value="3">
-                <input type="text" class="i-13" value="4">
-                <input type="text" class="i-13" value="5">
-
-                <button class="button-primary b-13">Task 13</button>
-                <div class="out-13"></div>
-            </section>
-
-            <section>
-                <div class="form-control"> <input type="radio" name="i-14" value="one" checked>1 </div>
-                <div class="form-control"> <input type="radio" name="i-14" value="two">2 </div>
-                <div class="form-control"> <input type="radio" name="i-14" value="three">3 </div>
-                <div class="form-control"> <input type="radio" name="i-14" value="four">4 </div>
-                <div class="form-control"> <input type="radio" name="i-14" value="five">5 </div>
-
-                <button class="button-primary b-14">Task 14</button>
-                <div class="out-14"></div>
-            </section>
-
-            <section>
-                <button class="button-primary b-15">Task 15</button>
-                <div class="out-15"></div>
-            </section>
-
-        
         </div>
-        
         
         
 
