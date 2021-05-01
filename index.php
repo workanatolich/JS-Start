394a9baec68090b28d8cfa7c0d553320ce34da08
anatolich <anatolich.work.space@gmail.com>
2021-05-01 22:30:09 +0300

    Lesson 7. Functions. Practice

--- index.php
+++ index.php
@@ -12,8 +12,59 @@
         <div class="container">
             <h1 class="text-center">Function</h1>
 
-            <button class="f-1">Func 1</button>
-            <button class="f-2">Func 2</button>
+            <section>
+                <button class="button-primary b-1">Task 1</button>
+                <div class="out-1"></div>
+            </section>
+
+            <section>
+                <button class="button-primary b-2">Task 2</button>
+                <div class="out-2"></div>
+            </section>
+
+            <section>
+                <button class="button-primary b-31">Task 3-1</button>
+                <button class="button-primary b-32">Task 3-2</button>
+                <div class="out-31"></div>
+                <div class="out-32"></div>
+            </section>
+
+            <section>
+                <button class="button-primary b-4">Task 4</button>
+                <div class="out-4"></div>
+            </section>
+
+            <section>
+                <button class="button-primary b-5">Task 5</button>
+                <div class="out-5"></div>
+            </section>
+
+            <section>
+                <button class="button-primary b-6">Task 6</button>
+                <div class="out-6"></div>
+            </section>
+
+            <section>
+                <button class="button-primary b-7">Task 7</button>
+                <div class="out-7"></div>
+            </section>
+
+            <section>
+                <button class="button-primary b-8">Task 8</button>
+                <div class="out-8"></div>
+            </section>
+
+            <section>
+                <button class="button-primary b-9">Task 9</button>
+                <div class="out-9"></div>
+            </section>
+
+            <section>
+                <button class="button-primary b-10">Task 10</button>
+                <div class="out-10"></div>
+            </section>
+
+
 
         </div>
         
--- js/script.js
+++ js/script.js
@@ -1,45 +1,77 @@
-let f1 = document.querySelector('.f-1');
+let a1 = 9;
+function t1() {
+    let out = document.querySelector('.out-1');
 
-function one() {
-    console.log("It's work");
+    out.innerHTML = a1;
+}
+document.querySelector('.b-1').onclick = t1;
+
+let a2 = 10;
+function t2() {
+    return a2;
+}
+document.querySelector('.b-2').onclick = () => {
+    document.querySelector('.out-2').textContent = t2();
 }
 
-one();
-
-f1.onclick = one;
-
-console.log(1+one()); // NaN
-console.log(one()); // underfined
-
-function two() {
-    console.log('work 1111');
-    return 54;
+function t3(a,b) {
+    return a*b;
+}
+document.querySelector('.b-31').onclick = () => {
+    document.querySelector('.out-31').textContent = t3(5,8);
+}
+document.querySelector('.b-32').onclick = () => {
+    document.querySelector('.out-32').textContent = t3(10, 1);
 }
 
-console.log(1+two());
-
-let a = 8;
-let b = 9;
-
-function multi() {
-    console.log(a*b);
-    return a * b;
+function t4(year) {
+    return 2021 - year;
+}
+document.querySelector('.b-4').onclick = () => {
+    document.querySelector('.out-4').textContent = t4(1998);
 }
 
-let c1 = multi();
-let c2 = 10 * multi();
-console.log(c1, c2);
-
-function multi2(x,y) {
-    return x * y;
+function t5(name) {
+    return `Hello ${name}`;
+}
+document.querySelector('.b-5').onclick = () => {
+    document.querySelector('.out-5').textContent = t5("User");
 }
 
-console.log(multi2(4,5));
-
-document.querySelector('.f-2').onclick = () => {
-    console.log ('*****');
+function t6(min, max) {
+    return Math.floor(Math.random() * (max-min)) + min ;
+}
+document.querySelector('.b-6').onclick = () => {
+    document.querySelector('.out-6').textContent = t6(1, 10);
 }
 
-document.querySelector('.f-2').onclick = function() {
-    console.log ('123*****');
+function t7() {
+    return `rgb(${t6(0,255)},${t6(0,255)},${t6(0,255)})`;
+}
+document.querySelector('.b-7').onclick = () => {
+    document.querySelector('.out-7').style.height = '30px';
+    document.querySelector('.out-7').style.background = t7();
+}
+
+function t8(str) {
+    return str.trim();
+}
+document.querySelector('.b-8').onclick = () => {
+    document.querySelector('.out-8').textContent = t8('            hello world ');
+}
+
+function t9(num) {
+    return num % 2 === 0;
+}
+document.querySelector('.b-9').onclick = () => {
+    document.querySelector('.out-9').textContent = t9(30);
+}
+
+function t10(a, b) {
+    if(a > b) {return a;}
+    else if (a < b) {return b;}
+    else {return a;}
+}
+document.querySelector('.b-10').onclick = () => {
+    document.querySelector('.out-10').textContent = t10(40, 40);
 }
