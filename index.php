28527112c642df143eda2b8096499f3b02124734
anatolich <anatolich.work.space@gmail.com>
2021-05-01 21:02:48 +0300

    Lesson 6. Nested loops. Practice

--- index.php
+++ index.php
@@ -12,7 +12,58 @@
         <div class="container">
             <h1 class="text-center">Working with nested loops</h1>
 
-            <div class="out"></div>
+            <section>
+                <button class = "button-primary b-1">Task 1</button>
+                <p class = "out-1"></p>
+            </section>
+
+            <section>
+                <button class="button-primary b-2">Task 2</button>
+                <p class="out-2"></p>
+            </section>
+
+            <section>
+                <button class="button-primary b-3">Task 3</button>
+                <p class="out-3"></p>
+            </section>
+
+            <section>
+                <button class="button-primary b-4">Task 4</button>
+                <p class="out-4"></p>
+            </section>
+
+            <section>
+                <button class="button-primary b-5">Task 5</button>
+                <p class="out-5"></p>
+            </section>
+
+            <section>
+                <button class="button-primary b-6">Task 6</button>
+                <p class="out-6"></p>
+            </section>
+
+            <section>
+                <button class="button-primary b-7">Task 7</button>
+                <p class="out-7"></p>
+            </section>
+
+            <section>
+                <button class="button-primary b-8">Task 8</button>
+                <p class="out-8"></p>
+            </section>
+
+            <section>
+                <button class="button-primary b-9">Task 9</button>
+                <p class="out-9"></p>
+            </section>
+
+            <section>
+                <button class="button-primary b-10">Task 10</button>
+                <p class="out-10"></p>
+            </section>
+
+
+
 
         </div>
         
--- js/script.js
+++ js/script.js
@@ -1,8 +1,137 @@
-let out = document.querySelector('.out');
+function f1() {
+    let out = document.querySelector('.out-1');
 
-for (let i = 1; i < 10; i++) {
-    for(let k = 1; k < 10; k++) {
-        out.innerHTML += `${i}*${k}=${i*k}<br>`;
+    for(let i = 0; i < 3; i++) {
+        for(let k = 0; k < 3; k++) {
+            out.innerHTML += '*';
+        }
+        out.innerHTML += '_';
     }
-    out.innerHTML += '<hr>';
 }
+document.querySelector('.b-1').onclick = f1;
+
+function f2() {
+    let out = document.querySelector('.out-2');
+
+    for(let i = 1; i < 4; i++) {
+        out.innerHTML += `<br> ${i} <br>`;
+        for(let k = 0; k < 3; k++) {
+            out.innerHTML += `*_`;
+        }
+    }
+}
+document.querySelector('.b-2').onclick = f2;
+
+function f3() {
+    let out = document.querySelector('.out-3');
+
+    for(let i = 1; i <= 4; i++) {
+        for(let k = 0; k < 3; k++) {
+            out.innerHTML += '*_';
+        }
+        out.innerHTML += '<br>';
+    }
+}
+document.querySelector('.b-3').onclick = f3;
+
+function f4() {
+    let out = document.querySelector('.out-4');
+
+    for(let i = 1; i <= 3; i++) {
+        out.innerHTML += i + '_';
+        for(let k = 1; k < 6; k++) {
+            out.innerHTML += k + ' ';
+        }
+    }
+}
+document.querySelector('.b-4').onclick = f4;
+
+function f5() {
+    let out = document.querySelector('.out-5');
+
+    for(let i = 1; i <= 3; i++) {
+        for(let k = 0; k < 6; k++) {
+            if(k % 2 !== 0 && k!== 0){
+                out.innerHTML += '0';
+            }
+            else {
+                out.innerHTML += '1';
+            }
+        }
+        out.innerHTML += '<br>';
+    }
+}
+document.querySelector('.b-5').onclick = f5;
+
+function f6() {
+    let out = document.querySelector('.out-6');
+
+    for(let i = 1; i <= 3; i++) {
+        for(let k = 0; k < 6; k++) {
+            if(k % 3 === 2) {
+                out.innerHTML += 'x';
+            }
+            else if(k % 2 !== 0) {
+                out.innerHTML += '0';
+            }
+            else {
+                out.innerHTML += '1';
+            }
+        }
+        out.innerHTML += '<br>';
+    }
+}
+document.querySelector('.b-6').onclick = f6;
+
+function f7() {
+    let out = document.querySelector('.out-7');
+
+    for(let i = 0; i < 5; i++) {
+        for(k = 0; k < i; k++) {
+            out.innerHTML += '*';
+        }
+        out.innerHTML += '<br>';
+    }
+}
+document.querySelector('.b-7').onclick = f7;
+
+function f8() {
+    let out = document.querySelector('.out-8');
+
+    for(let i = 5; i > 0; i--) {
+        for(k = 0; k < i; k++) {
+            out.innerHTML +='*';
+        }
+        out.innerHTML += '<br>';
+    }
+}
+document.querySelector('.b-8').onclick = f8;
+
+function f9() {
+    let out = document.querySelector('.out-9');
+
+    for(let i = 1; i < 7; i++) {
+        for(k = 1; k < i; k++) {
+            out.innerHTML += `${k} `;
+        }
+        out.innerHTML += '<br>';
+    }
+}
+document.querySelector('.b-9').onclick = f9;
+
+function f10() {
+    let out = document.querySelector('.out-10');
+
+    for(let i = 0; i < 6; i++) {
+        for(let k = 0; k <= 9; k++) {
+            if(k === 9) {
+                out.innerHTML += `${i+1}0`;
+                break;
+            }
+            out.innerHTML += `${i}${k+1} `;
+        }
+        out.innerHTML += '<br>'
+
+    }
+}
+document.querySelector('.b-10').onclick = f10;
