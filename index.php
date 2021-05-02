2ce2c30f76d8055a9e55a964c04314e23fb0b5af
anatolich <anatolich.work.space@gmail.com>
2021-05-02 17:53:50 +0300

    Lesson 8. Cycle 'While'. Theory

--- index.php
+++ index.php
@@ -6,70 +6,14 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <link rel="stylesheet" href="css/mustard-ui.min.css">
     <link rel="stylesheet" href="css/style.css"
-    <title>Lesson 7</title>
+    <title>Lesson 8</title>
 </head>
 <body>
         <div class="container">
-            <h1 class="text-center">Function</h1>
+            <h1 class="text-center">Cycle While</h1>
 
-            <section>
-                <button class="button-primary b-1">Task 1</button>
-                <div class="out-1"></div>
-            </section>
-
-            <section>
-                <button class="button-primary b-2">Task 2</button>
-                <div class="out-2"></div>
-            </section>
-
-            <section>
-                <button class="button-primary b-31">Task 3-1</button>
-                <button class="button-primary b-32">Task 3-2</button>
-                <div class="out-31"></div>
-                <div class="out-32"></div>
-            </section>
-
-            <section>
-                <button class="button-primary b-4">Task 4</button>
-                <div class="out-4"></div>
-            </section>
-
-            <section>
-                <button class="button-primary b-5">Task 5</button>
-                <div class="out-5"></div>
-            </section>
-
-            <section>
-                <button class="button-primary b-6">Task 6</button>
-                <div class="out-6"></div>
-            </section>
-
-            <section>
-                <button class="button-primary b-7">Task 7</button>
-                <div class="out-7"></div>
-            </section>
-
-            <section>
-                <button class="button-primary b-8">Task 8</button>
-                <div class="out-8"></div>
-            </section>
-
-            <section>
-                <button class="button-primary b-9">Task 9</button>
-                <div class="out-9"></div>
-            </section>
-
-            <section>
-                <button class="button-primary b-10">Task 10</button>
-                <div class="out-10"></div>
-            </section>
-
-
-
+            <div id="out"></div>
         </div>
-        
-        
-
     <script src="js/script.js"></script>
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,77 +1,35 @@
-let a1 = 9;
-function t1() {
-    let out = document.querySelector('.out-1');
-
-    out.innerHTML = a1;
-}
-document.querySelector('.b-1').onclick = t1;
-
-let a2 = 10;
-function t2() {
-    return a2;
-}
-document.querySelector('.b-2').onclick = () => {
-    document.querySelector('.out-2').textContent = t2();
+for (let i = 0; i < 6; i++) {
+    console.log(i);
 }
 
-function t3(a,b) {
-    return a*b;
-}
-document.querySelector('.b-31').onclick = () => {
-    document.querySelector('.out-31').textContent = t3(5,8);
-}
-document.querySelector('.b-32').onclick = () => {
-    document.querySelector('.out-32').textContent = t3(10, 1);
+let k = 0;
+while (k < 5) {
+    console.log('k: ' + k);
+    k++;
 }
 
-function t4(year) {
-    return 2021 - year;
+let sum = 0;
+let p = 0;
+while (p <= 20) {
+    sum = sum + p;
+    p++;
 }
-document.querySelector('.b-4').onclick = () => {
-    document.querySelector('.out-4').textContent = t4(1998);
-}
+console.log('sum = ' + sum);
 
-function t5(name) {
-    return `Hello ${name}`;
-}
-document.querySelector('.b-5').onclick = () => {
-    document.querySelector('.out-5').textContent = t5("User");
-}
+let out = document.querySelector('#out');
+p = 0;
+let outStr = '';
+let flag = 3;
 
-function t6(min, max) {
-    return Math.floor(Math.random() * (max-min)) + min ;
+while(p < 4) {
+    let p1 = 0;
+    while (p1 < 4) {
+        if (p1 < flag) { outStr += '0'; }
+        else { outStr += '*'; }
+        p1++;
+    }
+    flag--;
+    outStr += '<br>';
+    p++;
 }
-document.querySelector('.b-6').onclick = () => {
-    document.querySelector('.out-6').textContent = t6(1, 10);
-}
-
-function t7() {
-    return `rgb(${t6(0,255)},${t6(0,255)},${t6(0,255)})`;
-}
-document.querySelector('.b-7').onclick = () => {
-    document.querySelector('.out-7').style.height = '30px';
-    document.querySelector('.out-7').style.background = t7();
-}
-
-function t8(str) {
-    return str.trim();
-}
-document.querySelector('.b-8').onclick = () => {
-    document.querySelector('.out-8').textContent = t8('            hello world ');
-}
-
-function t9(num) {
-    return num % 2 === 0;
-}
-document.querySelector('.b-9').onclick = () => {
-    document.querySelector('.out-9').textContent = t9(30);
-}
-
-function t10(a, b) {
-    if(a > b) {return a;}
-    else if (a < b) {return b;}
-    else {return a;}
-}
-document.querySelector('.b-10').onclick = () => {
-    document.querySelector('.out-10').textContent = t10(40, 40);
-}
+out.innerHTML = outStr;
