123fa5ecbcbe59884d4413bc6a6f52b2f3c135ef
anatolich <anatolich.work.space@gmail.com>
2021-05-02 18:59:09 +0300

    Lesson 8. Cycle 'While'. Practice

--- index.php
+++ index.php
@@ -12,7 +12,159 @@
         <div class="container">
             <h1 class="text-center">Cycle While</h1>
 
-            <div id="out"></div>
+            <section>
+                <button class="button-primary b-1">Task 1</button>
+                <div class="out-1"></div>
+            </section>
+
+            <section>
+                <button class="button-primary b-2">Task 2</button>
+                <div class="out-2"></div>
+            </section>
+
+            <section>
+                <button class="button-primary b-3">Task 3</button>
+                <div class="out-3"></div>
+            </section>
+
+            <section>
+                <button class="button-primary b-4">Task 4</button>
+                <div class="out-4"></div>
+            </section>
+
+            <section>
+                <button class="button-primary b-5">Task 5</button>
+                <div class="out-5"></div>
+            </section>
+
+            <section>
+                <div class="form-control">
+                    Input i-6
+                    <label><input type="number" class="i-6"></label>
+                </div>
+
+                <button class="button-primary b-6">Task 6</button>
+                <div class="out-6"></div>
+            </section>
+
+            <section>
+                <div class="form-control">
+                    Input i-7
+                    <label><input type="number" class="i-7"></label>
+                </div>
+
+                <button class="button-primary b-7">Task 7</button>
+                <div class="out-7"></div>
+            </section>
+
+            <section>
+                <div class="form-control">
+                    Input i-81
+                    <label><input type="number" class="i-81"></label>
+                </div>
+
+                <div class="form-control">
+                    Input i-82
+                    <label><input type="number" class="i-82"></label>
+                </div>
+
+                <button class="button-primary b-8">Task 8</button>
+                <div class="out-8"></div>
+            </section>
+
+            <section>
+                <div class="form-control">
+                    Input i-91
+                    <label><input type="number" class="i-91"></label>
+                </div>
+
+                <div class="form-control">
+                    Input i-92
+                    <label><input type="number" class="i-92"></label>
+                </div>
+
+                <button class="button-primary b-9">Task 9</button>
+                <div class="out-9"></div>
+            </section>
+
+            <section>
+                <button class="button-primary b-10">Task 10</button>
+                <div class="out-10"></div>
+            </section>
+
+            <section>
+
+                <div class="div-11">one</div>
+                <div class="div-11">3</div>
+                <div class="div-11">4</div>
+                <div class="div-11">two</div>
+
+                <button class="button-primary b-11">Task 11</button>
+                <div class="out-11"></div>
+            </section>
+
+            <section>
+
+                <div class="div-12">Div 12</div>
+                <div class="div-12">Div 12</div>
+                <div class="div-12">Div 12</div>
+                <div class="div-12">Div 12</div>
+
+                <button class="button-primary b-12">Task 12</button>
+
+            </section>
+
+            <section>
+                <div class="form-control">
+                    Input i-13
+                    <label><input type="text" class="i-13" placeholder="Enter some info"></label>
+                </div>
+
+                <div class="form-control">
+                    Input i-13
+                    <label><input type="text" class="i-13" placeholder="Enter some info"></label>
+                </div>
+
+                <div class="form-control">
+                    Input i-13
+                    <label><input type="text" class="i-13" placeholder="Enter some info"></label>
+                </div>
+
+                <button class="button-primary b-13">Task 13</button>
+            </section>
+
+            <section>
+                <fieldset>
+                    <legend>Input i-14</legend>
+
+                    <div class="form-control">
+                        <label><input type="radio" name="i-14" class="i-14" value="1" checked>Radio</label>
+                    </div>
+
+                    <div class="form-control">
+                        <label><input type="radio" name="i-14" class="i-14" value="2">Radio</label>
+                    </div>
+
+                    <div class="form-control">
+                        <label><input type="radio" name="i-14" class="i-14" value="3">Radio</label>
+                    </div>
+
+                </fieldset>
+
+                <button class="button-primary b-14">Task 14</button>
+                <div class="out-14"></div>
+            </section>
+
+            <section>
+                <button class="button-primary b-15">Task 15</button>
+                <div class="out-15"></div>
+            </section>
+
+
+
+
+
+
         </div>
     <script src="js/script.js"></script>
 </body>
--- js/script.js
+++ js/script.js
@@ -1,35 +1,190 @@
-for (let i = 0; i < 6; i++) {
-    console.log(i);
-}
+function t1() {
+    let out = document.querySelector('.out-1');
+    let i = 1;
 
-let k = 0;
-while (k < 5) {
-    console.log('k: ' + k);
-    k++;
-}
-
-let sum = 0;
-let p = 0;
-while (p <= 20) {
-    sum = sum + p;
-    p++;
-}
-console.log('sum = ' + sum);
-
-let out = document.querySelector('#out');
-p = 0;
-let outStr = '';
-let flag = 3;
-
-while(p < 4) {
-    let p1 = 0;
-    while (p1 < 4) {
-        if (p1 < flag) { outStr += '0'; }
-        else { outStr += '*'; }
-        p1++;
+    while (i <= 50) {
+        out.innerHTML += `${i} `;
+        i++;
     }
-    flag--;
-    outStr += '<br>';
-    p++;
 }
-out.innerHTML = outStr;
+document.querySelector('.b-1').onclick = t1;
+
+function t2() {
+    let out = document.querySelector('.out-2');
+    let i = 2;
+
+    while(i <= 122) {
+        out.innerHTML += `${i} `;
+        i+=2;
+    }
+}
+document.querySelector('.b-2').onclick = t2;
+
+function t3() {
+    let out = document.querySelector('.out-3');
+    let i = 25;
+
+    while(i >= 7) {
+        out.innerHTML += `${i} `;
+        i--;
+    }
+}
+document.querySelector('.b-3').onclick = t3;
+
+function t4() {
+    let out = document.querySelector('.out-4');
+    let i = 77;
+
+    while(i >= 35) {
+        out.innerHTML += `${i}_`;
+        i-=3;
+    }
+}
+document.querySelector('.b-4').onclick = t4;
+
+function t5() {
+    let out = document.querySelector('.out-5');
+    let i = 1;
+
+    while(i <= 17) {
+        if(i % 2 === 0) { out.innerHTML += `${i}_**`; }
+        else { out.innerHTML += `${i}_*`; }
+        i++;
+    }
+}
+document.querySelector('.b-5').onclick = t5;
+
+function t6() {
+    let out = document.querySelector('.out-6');
+
+    let num = +document.querySelector('.i-6').value;
+    let i = 1;
+
+    while(i <= num) {
+        out.innerHTML += `****** <br>`;
+        i++;
+    }
+}
+document.querySelector('.b-6').onclick = t6;
+
+function t7() {
+    let out = document.querySelector('.out-7');
+
+    let num = +document.querySelector('.i-7').value;
+
+    while(num >= 0) {
+        out.innerHTML += `${num} `;
+        num--;
+    }
+}
+document.querySelector('.b-7').onclick = t7;
+
+function t8() {
+    let out = document.querySelector('.out-8');
+
+    let a = +document.querySelector('.i-81').value;
+    let b = +document.querySelector('.i-82').value;
+
+    while(a <= b) {
+        out.innerHTML += `${a} `;
+        a++;
+    }
+}
+document.querySelector('.b-8').onclick = t8;
+
+function t9() {
+    let out = document.querySelector('.out-9');
+
+    let a = +document.querySelector('.i-91').value;
+    let b = +document.querySelector('.i-92').value;
+
+    if(a>b) {
+        let c = b;
+        b = a;
+        a = c;
+    }
+
+    while(a <= b) {
+        out.innerHTML += `${a} `;
+        a++;
+    }
+}
+document.querySelector('.b-9').onclick = t9;
+
+function t10() {
+    let out = document.querySelector('.out-10');
+
+    let i = 1950;
+
+    while (i <= 2000) {
+        out.innerHTML += i + ' ';
+        i += 2;
+    }
+}
+document.querySelector('.b-10').onclick = t10;
+
+function t11() {
+    let out = document.querySelector('.out-11');
+
+    let elem = document.querySelectorAll('.div-11');
+    let i = 0;
+
+    while (i < elem.length) {
+        out.innerHTML += elem[i].innerHTML + ' ';
+        i++;
+    }
+}
+document.querySelector('.b-11').onclick = t11;
+
+function t12() {
+    let elem = document.querySelectorAll('.div-12');
+    let i = 0;
+
+    while (i < elem.length) {
+        elem[i].style.marginBottom = '20px';
+        elem[i].style.background = 'orange';
+        i++;
+    }
+}
+document.querySelector('.b-12').onclick = t12;
+
+function t13() {
+    let elem = document.querySelectorAll('.i-13');
+    let i = 0;
+
+    while (i < elem.length) {
+        elem[i].value = `${i+1}`;
+        i++;
+    }
+}
+document.querySelector('.b-13').onclick = t13;
+
+function t14() {
+    let out = document.querySelector('.out-14');
+
+    let elem = document.querySelectorAll('.i-14');
+    let i = 0;
+
+    while (i < elem.length) {
+        if(elem[i].checked) {
+            out.innerHTML = elem[i].value;
+        }
+        i++;
+    }
+}
+document.querySelector('.b-14').onclick = t14;
+
+function t15() {
+    let out = document.querySelector('.out-15');
+
+    let i = 0;
+
+    while (i <= 10) {
+        out.innerHTML += `${10-i} ${i} `;
+        i++;
+    }
+}
+document.querySelector('.b-15').onclick = t15;
+
+
+
