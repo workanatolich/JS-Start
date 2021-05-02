4caeb028b3b7bdab826ffadc70687c96cc877ab6
anatolich <anatolich.work.space@gmail.com>
2021-05-02 20:59:48 +0300

    Lesson 9. Introduction to the DOM. Theory

--- index.php
+++ index.php
@@ -6,164 +6,29 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <link rel="stylesheet" href="css/mustard-ui.min.css">
     <link rel="stylesheet" href="css/style.css"
-    <title>Lesson 8</title>
+    <title>Lesson 9</title>
 </head>
 <body>
         <div class="container">
-            <h1 class="text-center">Cycle While</h1>
+            <h1 class="text-center">Introduction to DOM</h1>
+
+            <button class="toggle">Toggle</button>
+            <p class="one" data="privet">Test</p>
 
             <section>
-                <button class="button-primary b-1">Task 1</button>
-                <div class="out-1"></div>
-            </section>
-
-            <section>
-                <button class="button-primary b-2">Task 2</button>
-                <div class="out-2"></div>
-            </section>
-
-            <section>
-                <button class="button-primary b-3">Task 3</button>
-                <div class="out-3"></div>
-            </section>
-
-            <section>
-                <button class="button-primary b-4">Task 4</button>
-                <div class="out-4"></div>
-            </section>
-
-            <section>
-                <button class="button-primary b-5">Task 5</button>
-                <div class="out-5"></div>
-            </section>
-
-            <section>
+                <p>Выберите тип бензина:</p>
                 <div class="form-control">
-                    Input i-6
-                    <label><input type="number" class="i-6"></label>
+                    Количество литров
+                    <label><input type="number" class="litres"></label>
                 </div>
 
-                <button class="button-primary b-6">Task 6</button>
-                <div class="out-6"></div>
-            </section>
-
-            <section>
-                <div class="form-control">
-                    Input i-7
-                    <label><input type="number" class="i-7"></label>
-                </div>
-
-                <button class="button-primary b-7">Task 7</button>
-                <div class="out-7"></div>
-            </section>
-
-            <section>
-                <div class="form-control">
-                    Input i-81
-                    <label><input type="number" class="i-81"></label>
-                </div>
-
-                <div class="form-control">
-                    Input i-82
-                    <label><input type="number" class="i-82"></label>
-                </div>
-
-                <button class="button-primary b-8">Task 8</button>
-                <div class="out-8"></div>
-            </section>
-
-            <section>
-                <div class="form-control">
-                    Input i-91
-                    <label><input type="number" class="i-91"></label>
-                </div>
-
-                <div class="form-control">
-                    Input i-92
-                    <label><input type="number" class="i-92"></label>
-                </div>
-
-                <button class="button-primary b-9">Task 9</button>
-                <div class="out-9"></div>
-            </section>
-
-            <section>
-                <button class="button-primary b-10">Task 10</button>
-                <div class="out-10"></div>
-            </section>
-
-            <section>
-
-                <div class="div-11">one</div>
-                <div class="div-11">3</div>
-                <div class="div-11">4</div>
-                <div class="div-11">two</div>
-
-                <button class="button-primary b-11">Task 11</button>
-                <div class="out-11"></div>
-            </section>
-
-            <section>
-
-                <div class="div-12">Div 12</div>
-                <div class="div-12">Div 12</div>
-                <div class="div-12">Div 12</div>
-                <div class="div-12">Div 12</div>
-
-                <button class="button-primary b-12">Task 12</button>
+                <button class="gas" data="1.2">А 92</button>
+                <button class="gas" data="1.6">А 95</button>
+                <button class="gas" data="1.9">А 98</button>
 
             </section>
 
-            <section>
-                <div class="form-control">
-                    Input i-13
-                    <label><input type="text" class="i-13" placeholder="Enter some info"></label>
-                </div>
-
-                <div class="form-control">
-                    Input i-13
-                    <label><input type="text" class="i-13" placeholder="Enter some info"></label>
-                </div>
-
-                <div class="form-control">
-                    Input i-13
-                    <label><input type="text" class="i-13" placeholder="Enter some info"></label>
-                </div>
-
-                <button class="button-primary b-13">Task 13</button>
-            </section>
-
-            <section>
-                <fieldset>
-                    <legend>Input i-14</legend>
-
-                    <div class="form-control">
-                        <label><input type="radio" name="i-14" class="i-14" value="1" checked>Radio</label>
-                    </div>
-
-                    <div class="form-control">
-                        <label><input type="radio" name="i-14" class="i-14" value="2">Radio</label>
-                    </div>
-
-                    <div class="form-control">
-                        <label><input type="radio" name="i-14" class="i-14" value="3">Radio</label>
-                    </div>
-
-                </fieldset>
-
-                <button class="button-primary b-14">Task 14</button>
-                <div class="out-14"></div>
-            </section>
-
-            <section>
-                <button class="button-primary b-15">Task 15</button>
-                <div class="out-15"></div>
-            </section>
-
-
-
-
-
+            <div class="test"></div>
 
         </div>
     <script src="js/script.js"></script>
