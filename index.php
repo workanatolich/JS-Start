19946d3e561e48ea91f76447096dc688c6637f46
anatolich <anatolich.work.space@gmail.com>
2021-05-03 19:28:58 +0300

    Lesson 10. Array in JS. Part 1. Practice

--- index.php
+++ index.php
@@ -11,6 +11,108 @@
 <body>
         <div class="container">
             <h1 class="text-center">Arrays in JS. Part 1. </h1>
+
+            <section>
+                <button class="button-primary b-1">Task 1</button>
+                <div class="out-1"></div>
+            </section>
+            
+            <section>
+                <button class="button-primary b-2">Task 2</button>
+                <div class="out-2"></div>
+            </section>
+            
+            <section>
+                <button class="button-primary b-3">Task 3</button>
+                <div class="out-3"></div>
+            </section>
+            
+            <section>
+                <button class="button-primary b-4">Task 4</button>
+                <div class="out-4"></div>
+            </section>
+            
+            <section>
+                <button class="button-primary b-5">Task 5</button>
+                <div class="out-5"></div>
+            </section>
+            
+            <section>
+                <button class="button-primary b-6">Task 6</button>
+                <div class="out-6"></div>
+            </section>
+            
+            <section>
+                <button class="button-primary b-7">Task 7</button>
+                <div class="out-7"></div>
+            </section>
+            
+            <section>
+                <button class="button-primary b-8">Task 8</button>
+                <div class="out-8"></div>
+            </section>
+            
+            <section>
+                <button class="button-primary b-9">Task 9</button>
+                <div class="out-9"></div>
+            </section>
+            
+            <section>
+                <button class="button-primary b-10">Task 10</button>
+                <div class="out-10"></div>
+            </section>
+            
+            <section>
+                <button class="button-primary b-11">Task 11</button>
+                <div class="out-11"></div>
+            </section>
+            
+            <section>
+                <button class="button-primary b-12">Task 12</button>
+                <div class="out-12"></div>
+            </section>
+            
+            <section>
+                <button class="button-primary b-13">Task 13</button>
+                <div class="out-13"></div>
+            </section>
+            
+            <section>
+                <button class="button-primary b-14">Task 14</button>
+                <div class="out-14"></div>
+            </section>
+            
+            <section>
+                <button class="button-primary b-15">Task 15</button>
+                <div class="out-15"></div>
+            </section>
+            
+            <section>
+                <button class="button-primary b-16">Task 16</button>
+                <div class="out-16_odd"></div>
+                <div class="out-16_even"></div>
+            </section>
+            
+            <section>
+                <button class="button-primary b-17">Task 17</button>
+                <div class="out-17"></div>
+            </section>
+            
+            <section>
+                <button class="button-primary b-18">Task 18</button>
+                <div class="out-18"></div>
+            </section>
+            
+            <section>
+                <button class="button-primary b-19">Task 19</button>
+                <div class="out-19"></div>
+            </section>
+            
+            <section>
+                <button class="button-primary b-20">Task 20</button>
+                <div class="out-20"></div>
+            </section>
+            
         </div>
     <script src="js/script.js"></script>
 </body>
