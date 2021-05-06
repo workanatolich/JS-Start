bc13c074e9bf13ceddf9f7d9c2f60acf27891cbf
anatolich <anatolich.work.space@gmail.com>
2021-05-06 16:34:06 +0300

    Lesson 12. Two dimensional arrays. Theory

--- index.php
+++ index.php
@@ -1,158 +1,23 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/mustard-ui.min.css">
-    <link rel="stylesheet" href="css/style.css"
-    <title>Lesson 11</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Lesson 12</title>
 </head>
 <body>
-        <div class="container">
-            <h1 class="text-center">Arrays in JS. Part 2. </h1>
 
-            <section>
-                <div class="form-control">
-                    Input i-1
-                    <label><input type="text" class="i-1"></label>
-                </div>
-                <button class="button-primary b-1">Task 1</button>
-                <div class="out-1"></div>
-            </section> 
-            
-            <section>
-                <button class="button-primary b-2">Task 2</button>
-                <div class="out-2"></div>
-            </section> 
-            
-            <section>
-                <button class="button-primary b-3">Task 3</button>
-                <div class="out-3"></div>
-            </section> 
-            
-            <section>
-                <div class="form-control">
-                    Push
-                    <label><input type="text" class="i-4"></label>
-                </div>
-                <button class="button-primary b-4">Task 4</button>
-                <div class="out-4"></div>
-            </section> 
-            
-            <section>
-                <div class="form-control">
-                    Unshift
-                    <label><input type="text" class="i-5"></label>
-                </div>
-                <button class="button-primary b-5">Task 5</button>
-                <div class="out-5"></div>
-            </section> 
-            
-            <section>
-                <div class="form-control">
-                    Эмуляция push
-                    <label><input type="text" class="i-6"></label>
-                </div>
-                <button class="button-primary b-6">Task 6</button>
-                <div class="out-6"></div>
-            </section> 
-            
-            <section>
-                <button class="button-primary b-7">Task 7</button>
-                <div class="out-7"></div>
-            </section> 
-            
-            <section>
-                <div class="form-control">
-                    Эмуляция unshift
-                    <label><input type="text" class="i-8"></label>
-                </div>
-                <button class="button-primary b-8">Task 8</button>
-                <div class="out-8"></div>
-            </section> 
-            
-            <section>
-                <button class="button-primary b-9">Task 9</button>
-                <div class="out-9"></div>
-            </section> 
-            
-            <section>
-                <button class="button-primary b-10">Task 10</button>
-                <div class="out-10"></div>
-            </section> 
-            
-            <section>
-                <div class="form-control">
-                    indexOf
-                    <label><input type="text" class="i-11"</label>
-                </div>
-                <button class="button-primary b-11">Task 11</button>
-                <div class="out-11"></div>
-            </section> 
-            
-            <section>
-                <div class="form-control">
-                    Эмуляция indexOf
-                    <label><input type="text" class="i-12"</label>
-                </div>
-                <button class="button-primary b-12">Task 12</button>
-                <div class="out-12"></div>
-            </section> 
-            
-            <section>
-                <button class="button-primary b-13">Task 13</button>
-                <div class="out-13"></div>
-            </section> 
-            
-            <section>
-                <div class="form-control">
-                    заполняем массив
-                    <label><input type="text" class="i-14"></label>
-                </div>
-                <button class="button-primary b-14">Task 14</button>
-                <div class="out-14"></div>
-            </section> 
-            
-            <section>
-                <div class="form-control">
-                    заполняем массив
-                    <label><input type="text" class="i-15"></label>
-                </div>
-                <button class="button-primary b-15">Task 15</button>
-                <div class="out-15"></div>
-            </section> 
-            
-            <section>
-                <button class="button-primary b-16">Task 16</button>
-                <div class="out-16"></div>
-            </section> 
-            
-            <section>
-                <button class="button-primary b-17">Task 17</button>
-                <div class="out-17"></div>
-            </section> 
-            
-            <section>
-                <div class="form-control">
-                    Заполняем массив
-                    <label><input type="text" class="i-18"></label>
-                </div>
-                <button class="button-primary b-18">Task 18</button>
-                <div class="out-18"></div>
-            </section> 
-            
-            <section>
-                <button class="button-primary b-19">Task 19</button>
-                <div class="out-19"></div>
-            </section> 
-            
-            <section>
-                <button class="button-primary b-20">Task 20</button>
-                <div class="out-20"></div>
-            </section>        
-            
-        </div>
+    <div class="container">
+        <h1 class="text-center">Two dimensional arrays</h1>
+
+        <div class="out-1"></div>
+    </div>
+
     <script src="js/script.js"></script>
+
 </body>
 </html>
