fdc68143d2f7ee368af5dc54f1cb6b34d07b4a97
anatolich <anatolich.work.space@gmail.com>
2021-05-08 16:15:19 +0300

    Lesson 13. Associative arrays. Theory

--- index.php
+++ index.php
@@ -6,85 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/mustard-ui.min.css">
     <link rel="stylesheet" href="css/style.css">
-    <title>Lesson 12</title>
+    <title>Lesson 13</title>
 </head>
 <body>
 <div class="container">
-    <h1 class="text-center">Two dimensional arrays</h1>
+    <h1 class="text-center">Associative arrays</h1>
 
-    <section>
-        <button class="button-primary b-1">Task 1</button>
-        <div class="out-1"></div>
-    </section>
-
-    <section>
-        <button class="button-primary b-2">Task 2</button>
-        <div class="out-2"></div>
-    </section>
-
-    <section>
-        <button class="button-primary b-3">Task 3</button>
-        <div class="out-3"></div>
-    </section>
-
-    <section>
-        <button class="button-primary b-4">Task 4</button>
-        <div class="out-4"></div>
-    </section>
-
-    <section>
-        <button class="button-primary b-5">Task 5</button>
-        <div class="out-5"></div>
-    </section>
-
-    <section>
-        <button class="button-primary b-6">Task 6</button>
-        <div class="out-6"></div>
-    </section>
-
-    <section>
-        <button class="button-primary b-7">Task 7</button>
-        <div class="out-7"></div>
-    </section>
-
-    <section>
-        <button class="button-primary b-8">Task 8</button>
-        <div class="out-8"></div>
-    </section>
-
-    <section>
-        <button class="button-primary b-9">Task 9</button>
-        <div class="out-9"></div>
-    </section>
-
-    <section>
-        <button class="button-primary b-10">Task 10</button>
-        <div class="out-10"></div>
-    </section>
-
-    <section>
-        <button class="button-primary b-11">Task 11</button>
-        <div class="out-11"></div>
-    </section>
-
-    <section>
-        <button class="button-primary b-12">Task 12</button>
-        <div class="out-12"></div>
-    </section>
-
-    <section>
-        <button class="button-primary b-13">Task 13</button>
-    </section>
-
-    <section>
-        <button class="button-primary b-14">Task 14</button>
-        <div class="out-14"></div>
-    </section>
-
-    <section>
-        <button class="button-primary b-15">Task 15</button>
-        <div class="out-15"></div>
-    </section>
 
     <script src="js/script.js"></script>
 </body>
