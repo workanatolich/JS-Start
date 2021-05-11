541cab96087c9d33b18409145562f5afe7ed7b45
anatolich <anatolich.work.space@gmail.com>
2021-05-12 01:46:57 +0300

    Lesson 14. Weather API. Theory + practice

--- index.php
+++ index.php
@@ -6,158 +6,25 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/mustard-ui.min.css">
     <link rel="stylesheet" href="css/style.css">
-    <title>Lesson 13</title>
+    <title>Lesson 14</title>
 </head>
 <body>
 <div class="container">
-    <h1 class="text-center">Associative arrays</h1>
+    <h1 class="text-center">The weather API</h1>
 
-    <section>
-        <button class="button-primary b-1">Task 1</button>
-        <div class="out-1"></div>
-    </section>
-    
-    <section>
-        <button class="button-primary b-2">Task 2</button>
-        <div class="out-2"></div>
-    </section>
-    
-    <section>
-        <button class="button-primary b-3">Task 3</button>
-        <div class="out-3"></div>
-    </section>
-    
-    <section>
-        <button class="button-primary b-4">Task 4</button>
-        <div class="out-4"></div>
-    </section>
-    
-    <section>
-        <button class="button-primary b-5">Task 5</button>
-        <div class="out-5"></div>
-    </section>
-    
-    <section>
-        <div class="form-control">
-            Input i-61
-            <label><input type="text" class="i-61"></label>
+    <div class="pricing-table row">
+        <div class="package featured">
+            <p class="package-name">Moscow</p>
+            <hr>
+            <p class="price">10&deg;</p>
+            <p class="disclaimer">Clouds</p>
+            <hr>
+            <ul class="features">
+                <li>Features 1</li>
+            </ul>
+            <button class="button-primary">Go</button>
         </div>
-
-        <div class="form-control">
-            Input i-62
-            <label><input type="text" class="i-62"></label>
-        </div>
-
-        <button class="button-primary b-6">Task 6</button>
-        <div class="out-6"></div>
-    </section>
-    
-    <section>
-        <div class="form-control">
-            Input i-7
-            <label><input type="text" class="i-7"></label>
-        </div>
-
-        <button class="button-primary b-7">Task 7</button>
-        <div class="out-7"></div>
-    </section>
-    
-    <section>
-        <div class="form-control">
-            Input i-8
-            <label><input type="text" class="i-8"></label>
-        </div>
-
-        <button class="button-primary b-8">Task 8</button>
-        <div class="out-8"></div>
-    </section>
-    
-    <section>
-        <div class="form-control">
-            Input i-9
-            <label><input type="text" class="i-9"></label>
-        </div>
-
-        <button class="button-primary b-9">Task 9</button>
-        <div class="out-9"></div>
-    </section>
-    
-    <section>
-        <button class="button-primary b-10">Task 10</button>
-        <div class="out-10"></div>
-    </section>
-    
-    <section>
-        <div class="form-control">
-            Input i-11
-            <label><input type="text" class="i-11"></label>
-        </div>
-
-        <button class="button-primary b-11">Task 11</button>
-        <div class="out-11"></div>
-    </section>
-    
-    <section>
-        <div class="form-control">
-            Input i-12
-            <label><input type="text" class="i-12"></label>
-        </div>
-
-        <button class="button-primary b-12">Task 12</button>
-        <div class="out-12"></div>
-    </section>
-    
-    <section>
-        <button class="button-primary b-13">Task 13</button>
-        <div class="out-13"></div>
-    </section>
-    
-    <section>
-        <button class="button-primary b-14">Task 14</button>
-        <div class="out-14"></div>
-    </section>
-    
-    <section>
-        <button class="button-primary b-15">Task 15</button>
-        <div class="out-15"></div>
-    </section>
-    
-    <section>
-        <button class="button-primary b-16">Task 16</button>
-        <div class="out-16"></div>
-    </section>
-    
-    <section>
-        <button class="button-primary b-17">Task 17</button>
-        <div class="out-17"></div>
-    </section>
-    
-    <section>
-        <div class="form-control">
-            Input i-18
-            <label><input type="text" class="i-18"></label>
-        </div>
-
-        <button class="button-primary b-18">Task 18</button>
-        <div class="out-18"></div>
-    </section>
-    
-    <section>
-        <div class="form-control">
-            Input i-19
-            <label><input type="text" class="i-19"></label>
-        </div>
-
-        <button class="button-primary b-19">Task 19</button>
-        <div class="out-19"></div>
-    </section>
-    
-    <section>
-        <button class="button-primary b-20">Task 20</button>
-        <div class="out-20"></div>
-    </section>
-    
-    
+    </div>
     <script src="js/script.js"></script>
 </body>
 </html>
