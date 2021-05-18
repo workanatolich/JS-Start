c1f6b38cd0bde81eafee08db3d66c92d0f64a581
anatolich.work.space@gmail.com <anatolich.work.space@gmail.com>
2021-05-18 22:43:43 +0300

    Lesson 16. Cycles 'for of' && 'for in'. Theory

--- index.php
+++ index.php
@@ -6,85 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/mustard-ui.min.css">
     <link rel="stylesheet" href="css/style.css">
-    <title>Lesson 15</title>
+    <title>Lesson 16</title>
 </head>
 <body>
 <div class="container">
-    <h1 class="text-center">Set in JS</h1>
+    <h1 class="text-center">Cycles for of and for in</h1>
 
-    <section>
-        <div class="form-control">
-            Input i-2
-            <label><input type="text" class="i-2"></label>
-        </div>
-        <button class="button-primary b-2">Task 2</button>
-    </section>
-
-    <section>
-        <div class="form-control">
-            Input i-3
-            <label><input type="text" class="i-3"></label>
-        </div>
-        <button class="button-primary b-3">Task 3</button>
-    </section>
-
-    <section>
-        <div class="form-control">
-            Input i-4
-            <label><input type="text" class="i-4"></label>
-        </div>
-        <button class="button-primary b-4">Task 4</button>
-        <div class="out-4"></div>
-    </section>
-
-    <section>
-        <button class="button-primary b-5">Task 5</button>
-        <div class="out-5"></div>
-    </section>
-
-    <section>
-        <button class="button-primary b-6">Task 6</button>
-        <div class="out-6"></div>
-    </section>
-
-    <section>
-        <div class="form-control">
-            Input i-7
-            <label><input type="text" class="i-7"></label>
-        </div>
-        <button class="button-primary b-7">Task 7</button>
-        <div class="out-7"></div>
-    </section>
-
-    <section>
-        <button class="button-primary b-8">Task 8</button>
-        <div class="out-8"></div>
-    </section>
-
-    <section>
-        <button class="button-primary b-9">Task 9</button>
-        <div class="out-9"></div>
-    </section>
-
-    <section>
-        <button class="button-primary b-10">Task 10</button>
-        <div class="out-10"></div>
-    </section>
-
-    <section>
-        <button class="button-primary b-11">Task 11</button>
-        <div class="out-11"></div>
-    </section>
-
-    <section>
-        <button class="button-primary b-12">Task 12</button>
-        <div class="out-12"></div>
-    </section>
-
-    <section>
-        <button class="button-primary b-13">Task 13</button>
-        <div class="out-13"></div>
-    </section>
+    <p>1</p>
+    <p>2</p>
+    <p>3</p>
 
 </div>
     <script src="js/script.js"></script>
