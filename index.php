2e559bdf53e099356d726b50afa4b92013cae8f5
anatolich.work.space@gmail.com <anatolich.work.space@gmail.com>
2021-05-25 02:30:38 +0300

    Lesson 18 . Split.Join.ForEach. Theory

--- index.php
+++ index.php
@@ -6,43 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/mustard-ui.min.css">
     <link rel="stylesheet" href="css/style.css">
-    <title>Lesson 17</title>
+    <title>Lesson 18</title>
 </head>
 <body>
 <div class="container">
-    <h1 class="text-center">Map && Filter</h1>
-
-    <section>
-        <button class="button-primary b-1">Task 1</button>
-    </section>
-    <section>
-        <button class="button-primary b-2">Task 2</button>
-    </section>
-    <section>
-        <button class="button-primary b-3">Task 3</button>
-    </section>
-    <section>
-        <button class="button-primary b-4">Task 4</button>
-    </section>
-    <section>
-        <button class="button-primary b-5">Task 5</button>
-    </section>
-    <section>
-        <button class="button-primary b-6">Task 6</button>
-    </section>
-    <section>
-        <button class="button-primary b-7">Task 7</button>
-    </section>
-    <section>
-        <button class="button-primary b-8">Task 8</button>
-    </section>
-    <section>
-        <button class="button-primary b-9">Task 9</button>
-    </section>
-    <section>
-        <button class="button-primary b-10">Task 10</button>
-
-    </section>
+    <h1 class="text-center">Split. Join. ForEach</h1>
 
 </div>
     <script src="js/script.js"></script>
