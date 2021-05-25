5325fb0ac44bc71d3e8d9bb27e678b511b15d0f2
anatolich.work.space@gmail.com <anatolich.work.space@gmail.com>
2021-05-25 17:43:17 +0300

    Lesson 19. Mouse events. Practice

--- index.php
+++ index.php
@@ -11,13 +11,84 @@
 <body>
 <div class="container">
     <h1 class="text-center">Mouse events</h1>
-
-    <div class="block one">one</div>
-    <div class="block two">two</div>
-    <div class="block three">three</div>
-
-    <progress value="10" max="100"></progress>
-    <button>Click</button>
+    <section>
+        <div class="div-1 block">Task-1</div>
+        <div class="out-1"></div>
+    </section>
+    <section>
+        <div class="div-2 block">Task-2</div>
+        <div class="out-2"></div>
+    </section>
+    <section>
+        <div class="div-3 block">Task-3</div>
+        <div class="out-3"></div>
+    </section>
+    <section>
+        <div class="div-4 block">Task 4</div>
+        <div class="out-4"></div>
+    </section>
+    <section>
+        <div class="div-5 active block">Task 5</div>
+    </section>
+    <section>
+        <div class="div-6 block">Task 6</div>
+        <ul class="ul-6 hide">
+            <li>one</li>
+            <li>two</li>
+            <li>three</li>
+        </ul>
+    </section>
+    <section>
+        <div class="div-7 block">Task 7</div>
+        <div class="out-7"></div>
+    </section>
+    <section>
+        <input type="checkbox" class="ch-8">Включить/выключить контекстное меню
+    </section>
+    <section>
+        <div class="div-9 block">
+            <img src="img/1.png">
+        </div>
+    </section>
+    <section>
+        <div class="div-10 block">
+            <img src="img/1.png">
+        </div>
+    </section>
+    <section>
+        <div class="div-11 block">
+            <img src="img/1.png">
+        </div>
+    </section>
+    <section>
+        <div class="div-12 block">Task 12</div>
+    </section>
+    <section>
+        <div class="div-13 block">Task 13</div>
+    </section>
+    <section>
+        <button class="button-primary b-14">Task 14</button>
+        <div class="div-14 block">Task 14</div>
+    </section>
+    <section>
+        <div class="div-15 block">1</div>
+    </section>
+    <section>
+        <div class="div-16 block">Task 16</div>
+    </section>
+    <section>
+        <button class="button-primary b-17_on">On</button>
+        <button class="button-primary b-17_off">Off</button>
+    </section>
+    <section>
+        <div class="div-18 block">Task 18</div>
+    </section>
+    <section>
+        <div class="div-19 block">Task 19</div>
+    </section>
+    <section>
+        <progress value="10" max="100"></progress>
+    </section>
 </div>
 
     <script src="js/script.js"></script>
--- js/script.js
+++ js/script.js
@@ -1,44 +1,184 @@
-document.querySelector('.one').onclick = function (event) {
-    console.log(event);
-    console.log('click');
+function t1() {
+    let text = document.querySelector('.div-1').textContent;
+    document.querySelector('.out-1').innerHTML = text;
 }
 
-document.querySelector('.two').ondblclick = () => {
-    console.log('double click');
+document.querySelector('.div-1').onclick = t1;
+
+function t2(event) {
+    document.querySelector('.out-2').innerHTML = event.altKey;
 }
 
-document.querySelector('.two').oncontextmenu = () => {
-    console.log('right bottom');
-    return false;
+document.querySelector('.div-2').onclick = t2;
+
+let w = 75;
+function t3() {
+    w+=5;
+    this.style.width = w + 'px';
+
+    document.querySelector('.out-3').innerHTML = w + 'px';
 }
 
-// let w = 75;
-// document.querySelector('.three').onmousemove = () => {
-//     document.querySelector('.three').style.width = w + 'px';
-//     w++;
-// }
+document.querySelector('.div-3').onclick = t3;
 
-document.querySelector('.three').onmouseenter = () => {
-    document.querySelector('.three').style.background = 'blue';
+function t4() {
+    let text = document.querySelector('.div-4').textContent;
+    document.querySelector('.out-4').innerHTML = text;
 }
 
-document.querySelector('.three').onmouseleave = () => {
-    document.querySelector('.three').style.background = 'blueviolet';
+document.querySelector('.div-4').ondblclick = t4;
+
+function t5() {
+    let classList = document.querySelector('.div-5').classList;
+    if(classList.contains('active')) {
+        classList.remove('active');
+    } else {
+        classList.add('active');
+    }
+
 }
 
-document.querySelector('.three').onmousedown = () => {
-    document.querySelector('.three').style.background = 'red';
+document.querySelector('.div-5').ondblclick = t5;
+
+function t6() {
+    let classList = document.querySelector('.ul-6').classList;
+    if(classList.contains('hide')) {
+        classList.remove('hide');
+    } else {
+        classList.add('hide');
+    }
+
 }
 
-document.querySelector('.three').onmouseup = () => {
-    document.querySelector('.three').style.background = 'green';
+document.querySelector('.div-6').ondblclick = t6;
+
+function t7() {
+    let classList = this.classList;
+    if(!(classList.contains('active'))) {
+        classList.add('active');
+    } else {
+        classList.remove('active');
+    }
+
 }
 
-let p = 10;
-document.querySelector('button').onclick = () => {
-    p++;
-    document.querySelector('progress').value = p;
+document.querySelector('.div-7').oncontextmenu = t7;
+
+function t8() {
+    let classList = this.classList;
+
+    classList.toggle('contextMenu');
+
+    if(classList.contains('contextMenu')) {
+        this.oncontextmenu = () => {
+            return true;
+        }
+    }
+    else {
+        this.oncontextmenu = () => {
+            return false;
+        }
+    }
+
 }
+
+document.querySelector('.ch-8').onchange = t8;
+
+function t9() {
+    let img = document.querySelector('.div-9 img');
+    img.setAttribute('src', 'img/2.png');
+}
+
+document.querySelector('.div-9').oncontextmenu = t9;
+
+function t10() {
+    let img = document.querySelector('.div-10 img');
+    img.setAttribute('src', 'img/2.png');
+}
+
+document.querySelector('.div-10').onmouseenter = t10;
+
+function t11() {
+    let img = document.querySelector('.div-11 img');
+    if(event.type == 'mouseenter') {
+        img.setAttribute('src','img/2.png');
+    } else {
+        img.setAttribute('src', 'img/1.png');
+    }
+}
+
+document.querySelector('.div-11').onmouseenter = t11;
+document.querySelector('.div-11').onmouseleave = t11;
+
+function t12() {
+    this.classList.add('active');
+}
+
+document.querySelector('.div-12').onmousedown = t12;
+
+function t13() {
+    if(event.type == 'mousedown') {
+        this.classList.add('active');
+    } else {
+        this.classList.remove('active');
+    }
+}
+
+document.querySelector('.div-13').onmousedown = t13;
+document.querySelector('.div-13').onmouseup = t13;
+
+function t14() {
+    document.querySelector('.div-14').classList.add('active');
+}
+
+document.querySelector('.b-14').onclick = t14;
+
+function t15() {
+    this.textContent ++;
+}
+
+document.querySelector('.div-15').onmousemove = t15;
+
+let widthDiv16 = 75;
+function t16() {
+    widthDiv16++;
+    this.style.width = widthDiv16 + 'px';
+}
+
+document.querySelector('.div-16').onmousemove = t16;
+
+function t17On() {
+    document.querySelector('.div-16').onmousemove = t16;
+}
+
+document.querySelector('.b-17_on').onclick = t17On;
+
+function t17Off() {
+    document.querySelector('.div-16').onmousemove = false;
+}
+
+document.querySelector('.b-17_off').onclick = t17Off;
+
+function t18() {
+    this.textContent = getComputedStyle(this).width;
+}
+
+document.querySelector('.div-18').onmouseenter = t18;
+
+function t19() {
+    this.textContent = this.classList;
+}
+
+document.querySelector('.div-19').onmouseout = t19;
+
+function t20() {
+    this.value++;
+}
+
+document.querySelector('progress').onmousemove = t20;
+
+
+
 
 
 
