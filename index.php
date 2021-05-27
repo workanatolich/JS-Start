5a9b9f79f0f06b9eba61d732c843f05b0cf1c905
anatolich.work.space@gmail.com <anatolich.work.space@gmail.com>
2021-05-27 17:34:12 +0300

    Lesson 20. Keyboard events. Theory

--- index.php
+++ index.php
@@ -6,88 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/mustard-ui.min.css">
     <link rel="stylesheet" href="css/style.css">
-    <title>Lesson 19</title>
+    <title>Lesson 20</title>
 </head>
 <body>
 <div class="container">
-    <h1 class="text-center">Mouse events</h1>
+    <h1 class="text-center">Keyboard events</h1>
+
     <section>
-        <div class="div-1 block">Task-1</div>
-        <div class="out-1"></div>
-    </section>
-    <section>
-        <div class="div-2 block">Task-2</div>
-        <div class="out-2"></div>
-    </section>
-    <section>
-        <div class="div-3 block">Task-3</div>
-        <div class="out-3"></div>
-    </section>
-    <section>
-        <div class="div-4 block">Task 4</div>
-        <div class="out-4"></div>
-    </section>
-    <section>
-        <div class="div-5 active block">Task 5</div>
-    </section>
-    <section>
-        <div class="div-6 block">Task 6</div>
-        <ul class="ul-6 hide">
-            <li>one</li>
-            <li>two</li>
-            <li>three</li>
-        </ul>
-    </section>
-    <section>
-        <div class="div-7 block">Task 7</div>
-        <div class="out-7"></div>
-    </section>
-    <section>
-        <input type="checkbox" class="ch-8">Включить/выключить контекстное меню
-    </section>
-    <section>
-        <div class="div-9 block">
-            <img src="img/1.png">
-        </div>
-    </section>
-    <section>
-        <div class="div-10 block">
-            <img src="img/1.png">
-        </div>
-    </section>
-    <section>
-        <div class="div-11 block">
-            <img src="img/1.png">
-        </div>
-    </section>
-    <section>
-        <div class="div-12 block">Task 12</div>
-    </section>
-    <section>
-        <div class="div-13 block">Task 13</div>
-    </section>
-    <section>
-        <button class="button-primary b-14">Task 14</button>
-        <div class="div-14 block">Task 14</div>
-    </section>
-    <section>
-        <div class="div-15 block">1</div>
-    </section>
-    <section>
-        <div class="div-16 block">Task 16</div>
-    </section>
-    <section>
-        <button class="button-primary b-17_on">On</button>
-        <button class="button-primary b-17_off">Off</button>
-    </section>
-    <section>
-        <div class="div-18 block">Task 18</div>
-    </section>
-    <section>
-        <div class="div-19 block">Task 19</div>
-    </section>
-    <section>
-        <progress value="10" max="100"></progress>
+        <div class="form-control"> Our input
+            <label><input type="text" class="i-1"></label></div>
     </section>
 </div>
 
