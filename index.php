79c78b29afdffc924b9ee702d6625b6268303aad
anatolich.work.space@gmail.com <anatolich.work.space@gmail.com>
2021-05-29 04:08:39 +0300

    Lesson 20. Keyboard events. Practice

--- index.php
+++ index.php
@@ -13,8 +13,141 @@
     <h1 class="text-center">Keyboard events</h1>
 
     <section>
-        <div class="form-control"> Our input
-            <label><input type="text" class="i-1"></label></div>
+        <div class="form-control">
+            <label>
+                Input i-1
+                <input type="text" class="i-1">
+            </label>
+            <div class="out-1"></div>
+        </div>
+    </section>
+    <section>
+        <div class="form-control">
+            <label>
+                Input i-2
+                <input type="text" class="i-2">
+            </label>
+            <div class="out-2"></div>
+        </div>
+    </section>
+    <section>
+        <div class="form-control">
+            <label>
+                Input i-3
+                <input type="text" class="i-3">
+            </label>
+            <div class="out-3"></div>
+        </div>
+    </section>
+    <section>
+        <div class="form-control">
+            <label>
+                Input i-4
+                <input type="text" class="i-4">
+            </label>
+            <div class="out-4"></div>
+        </div>
+    </section>
+    <section>
+        <div class="form-control">
+            <label>
+                Input i-5
+                <input type="text" class="i-5">
+            </label>
+            <div class="out-5"></div>
+        </div>
+    </section>
+    <section>
+        <div class="form-control">
+            <label>
+                Input i-6
+                <input type="text" class="i-6">
+            </label>
+            <div class="out-6"></div>
+        </div>
+    </section>
+    <section>
+        <div class="form-control">
+            <label>
+                Input i-7
+                <input type="text" class="i-7">
+            </label>
+            <div class="out-7"></div>
+        </div>
+    </section>
+    <section>
+        <div class="form-control">
+            <label>
+                Input i-8
+                <input type="text" class="i-8">
+            </label>
+            <div class="out-8"></div>
+        </div>
+    </section>
+    <section>
+        <div class="form-control">
+            <label>
+                Input i-9
+                <input type="text" class="i-9">
+            </label>
+            <div class="out-9"></div>
+        </div>
+    </section>
+    <section>
+        <div class="form-control">
+            <label>
+                Input i-10
+                <input type="text" class="i-10">
+            </label>
+            <img src="img/1.png" class="img-10 img-thumbnail">
+        </div>
+    </section>
+
+    <section>
+        <div class="form-control">
+            <label>
+                Input i-11
+                <input type="text" class="i-11">
+            </label>
+        </div>
+    </section>
+
+    <section>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="keyboard keyboard-1">1</div>
+                <div class="keyboard keyboard-2">2</div>
+                <div class="keyboard keyboard-3">3</div>
+                <div class="keyboard keyboard-4">4</div>
+                <div class="keyboard keyboard-5">5</div>
+                <div class="keyboard keyboard-6">6</div>
+                <div class="keyboard keyboard-7">7</div>
+                <div class="keyboard keyboard-8">8</div>
+                <div class="keyboard keyboard-9">9</div>
+                <div class="keyboard keyboard-0">0</div>
+            </div>
+
+            <div class="col-md-12">
+                <div class="keyboard keyboard-q">q</div>
+                <div class="keyboard keyboard-w">w</div>
+                <div class="keyboard keyboard-e">e</div>
+                <div class="keyboard keyboard-r">r</div>
+                <div class="keyboard keyboard-t">t</div>
+                <div class="keyboard keyboard-y">y</div>
+                <div class="keyboard keyboard-u">u</div>
+                <div class="keyboard keyboard-i">i</div>
+                <div class="keyboard keyboard-o">o</div>
+                <div class="keyboard keyboard-p">p</div>
+            </div>
+
+            <div class="col-md-12">
+                <div class="keyboard keyboard-shiftLeft">LShift</div>
+                <div class="keyboard keyboard-controlLeft">LCtrl</div>
+                <div class="keyboard keyboard-altLeft">LAlt</div>
+                <div class="keyboard keyboard-space">Space</div>
+                <div class="keyboard keyboard-enter">Enter</div>
+            </div>
+        </div>
     </section>
 </div>
 
--- js/script.js
+++ js/script.js
@@ -1,29 +1,197 @@
-document.querySelector('.i-1').onkeydown = function (event) {
-    console.log('keydown');
-    console.log('charCode: ' + event.charCode);
-    console.log('code: ' + event.code);
-    console.log('key: ' + event.key);
-    console.log('keyCode: ' + event.keyCode);
-
-    //console.log(event);
+function t1() {
+    document.querySelector('.out-1').innerHTML = event.key;
 }
 
-document.querySelector('.i-1').onkeypress = function (event) {
-    console.log('keypress');
-    console.log('charCode: ' + event.charCode);
-    console.log('code: ' + event.code);
-    console.log('key: ' + event.key);
-    console.log('keyCode: ' + event.keyCode);
+document.querySelector('.i-1').onkeypress = t1;
 
-    //console.log(event);
+function t2() {
+    document.querySelector('.out-2').innerHTML = `charCode: ${event.charCode} keyCode: ${event.keyCode}`;
 }
 
-document.querySelector('.i-1').onkeyup = function (event) {
-    console.log('keyup');
-    console.log('charCode: ' + event.charCode);
-    console.log('code: ' + event.code);
-    console.log('key: ' + event.key);
-    console.log('keyCode: ' + event.keyCode);
+document.querySelector('.i-2').onkeypress = t2;
 
-    //console.log(event);
+function t3() {
+    let min = 48;
+    let max = 57;
+
+    let out = '';
+    out = event.keyCode < min || event.keyCode > max;
+
+    document.querySelector('.out-3').innerHTML = out;
 }
+
+document.querySelector('.i-3').onkeypress = t3;
+
+let out4 = '';
+function t4() {
+
+    let alf = {a:97, b:98, c:99, d:100, e:101, f:102, g:103,
+        h:104, i:105, j:106, k:107, l:108, m:109, n:110,
+        o:111, p:112, q:113, r:114, s:115, t:116, u:117,
+        v:118, w:119, x:120, y:121, z:122};
+
+    let upperMin = 65;
+    let upperMax = 90;
+
+    let keyCode = '';
+
+    if(event.keyCode >= upperMin && event.keyCode <= upperMax) {
+        keyCode = event.keyCode + 32;
+        for(let key in alf) {
+            if(alf[key] == keyCode) {
+               return key;
+            }
+        }
+
+    } else {
+        return event.key;
+    }
+
+}
+
+document.querySelector('.i-4').onkeypress = () => {
+    out4 += t4();
+    document.querySelector('.out-4').innerHTML = out4;
+}
+
+let out5 = '';
+function t5() {
+
+    let alf = {A:65, B:66, C:67, D:68, E:69, F:70,
+        G:71, H:72, I:73, J:74, K:75, L:76, M:77,
+        N:78, O:79, P:80, Q:81, R:82, S:83, T:84,
+        U:85, V:86, W:87, X:88, Y:89, Z:90};
+
+    let lowerMin = 97;
+    let lowerMax = 122;
+
+    let keyCode = '';
+
+    if(event.keyCode >= lowerMin && event.keyCode <= lowerMax) {
+        keyCode = event.keyCode - 32;
+        for(let key in alf) {
+            if(alf[key] == keyCode) {
+               return key;
+            }
+        }
+
+    } else {
+        return event.key;
+    }
+
+}
+
+document.querySelector('.i-5').onkeypress = () => {
+    out5 += t5();
+    document.querySelector('.out-5').innerHTML = out5;
+}
+
+
+let out6 = '';
+
+document.querySelector('.i-6').onkeypress = () => {
+    out6 += t4();
+    document.querySelector('.i-6').value = out6;
+}
+
+
+function t7() {
+
+    const a7 = [];
+    a7.push('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A',
+        'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+        'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '@', '#',
+        '$', '%', '&', '?', '-', '+', '=', '~', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+    let num = Math.floor(Math.random() * a7.length);
+    let out = '';
+
+    for(let key in a7) {
+        if(key == num) {
+            out = a7[key];
+        }
+    }
+
+    document.querySelector('.out-7').innerHTML = out;
+}
+
+document.querySelector('.i-7').onkeypress = t7;
+
+
+let out8 = '';
+function t8() {
+    let key = event.key;
+    if(key == 'i') {
+        out8 += 1;
+    } else if(key == 'o') {
+        out8 += 0;
+    } else if(key == 'l') {
+        out8 += 7;
+    } else {
+        out8 += key;
+    }
+
+    document.querySelector('.out-8').innerHTML = out8;
+}
+
+document.querySelector('.i-8').onkeypress = t8;
+
+let out9 = 0;
+function t9() {
+    if(event.key == 'ArrowDown') {
+        out9 += 1;
+    }
+
+    document.querySelector('.out-9').innerHTML = out9;
+}
+
+document.querySelector('.i-9').onkeydown = t9;
+
+let width10 = 50;
+let height10 = 50;
+function t10() {
+    let img = document.querySelector('.img-10');
+    if(event.key == 'ArrowLeft' || event.key == 'ArrowRight') {
+        width10 ++;
+        img.style.width = width10 + 'px';
+    } else if(event.key == 'ArrowDown' || event.key == 'ArrowUp') {
+        height10 ++;
+        img.style.height = height10 + 'px';
+    }
+}
+
+document.querySelector('.i-10').onkeydown = t10;
+
+function t11() {
+    let set = new Set (['1','2','3','4','5','6',
+        '7','8','9','0', 'q','w','e','r','t','y', 'u',
+        'i','o','p', 'shiftleft', 'controlleft', 'altleft', 'space', 'enter']);
+
+    let out = '';
+    let elem;
+    function clearCode(out) {
+       let code = event.code;
+
+       if(code.includes('Digit')) {
+           out = code.substr(-1);
+       } else if(code.includes('Key')) {
+           out = code.substr(-1);
+       } else {
+           out = code;
+       }
+
+       return out;
+    }
+
+    let lowerOut = clearCode(out).toLowerCase();
+    if(set.has(lowerOut)) {
+        elem = document.querySelector(`.keyboard-${lowerOut}`);
+        elem.classList.toggle('active');
+    }
+}
+
+document.querySelector('.i-11').onkeydown = t11;
+
+
+
